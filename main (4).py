63e65aab8cd1dcf719abaefdfcba7cf907161fb0
Aaliya2004601 <148326039+Aaliya2004601@users.noreply.github.com>
2023-10-18 14:05:56 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,57 @@
+#3.1 Write a function called linear_search_product that take
+
+
+
+def linear_search_product(product_list, target_product):
+
+
+
+    indices = []
+
+
+
+    for i, product in enumerate(product_list):
+
+
+
+        if product == target_product:
+
+
+
+            indices.append(i)
+
+
+
+    return indices
+
+
+
+# Example usage:
+
+
+
+products = ["apple", "banana", "apple", "orange", "apple"]
+
+
+
+target = "apple"
+
+
+
+result = linear_search_product(products, target)
+
+
+
+if result:
+
+
+
+    print(f"The product '{target}' was found at indices: {result}")
+
+
+
+else:
+
+
+
+    print(f"The product '{target}' was not found in the list.")
